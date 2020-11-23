0b00c3747643129c4586ae4e37c305932ab9022c
Tiago Santos <santos.t.j.dos@gmail.com>
2020-11-23 17:21:36 -0300

    store

--- src/main/java/com/project/professor/allocation/dto/CourseCreationDTO.java
+++ src/main/java/com/project/professor/allocation/dto/CourseCreationDTO.java
@@ -1,11 +1,9 @@
 package com.project.professor.allocation.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode(callSuper = true)
