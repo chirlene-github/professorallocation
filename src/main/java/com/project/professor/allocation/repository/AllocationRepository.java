8b32aa4cac210341ea6910b129f8a8e176df0dbc
Tiago Santos <santos.t.j.dos@gmail.com>
2020-12-03 20:28:59 -0300

    Repository layer - Define extension methods

--- src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
@@ -1,10 +1,17 @@
 package com.project.professor.allocation.repository;
 
 import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.entity.Course;
+import com.project.professor.allocation.entity.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AllocationRepository extends JpaRepository<Allocation, Long> {
 
+    List<Allocation> findByProfessor(Professor professor);
+
+    List<Allocation> findByCourse(Course course);
 }
